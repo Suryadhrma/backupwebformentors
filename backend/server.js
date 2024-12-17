f81d662393e2c4f494cdda06f34de0d7902968c2
srya <m.surya.dharma9f@gmail.com>
2024-12-17 11:49:02 +0700

    InimahBackend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const user = require('./models/user')
+
+// Load environment variables
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.json());
+// Middleware
+app.use(express.json()); // Untuk menangani JSON payload
+app.use(express.urlencoded({ extended: true })); // Untuk menangani form data
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
+// Koneksi ke MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+  
+
+
+    const userRoutes = require('./routes/userroutes');
+    app.use(userRoutes);
+
+
+// Jalankan server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
