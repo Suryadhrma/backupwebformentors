f924be65e1a65a7e5a999a6a6d11dfb9ef494406
AlhadiDewana <aldialhadi9@gmail.com>
2024-12-18 11:34:19 +0700

    baru dikit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import CourseMentor from './pages/mentor/mentor-course/CourseMentor';
 import CreateCourse from './pages/mentor/mentor-create-course/CreateCourseMentor';
 import AddMateriMentor from './pages/mentor/mentor-create-course/AddMateriMentor';
 import SilabusMentor from './pages/mentor/mentor-create-course/SilabusMentor';
+import ChatMentor from './pages/mentor/chat/ChatMentor';
 
 
     function App() {
@@ -17,6 +18,7 @@ import SilabusMentor from './pages/mentor/mentor-create-course/SilabusMentor';
                 <Route path="/mentor/create-course" element={<CreateCourse />} />
                 <Route path="/mentor/materi-pembelajaran" element={<AddMateriMentor />} />
                 <Route path="/mentor/isi-silabus" element={<SilabusMentor />} />
+                <Route path="/mentor/chat" element={<ChatMentor />} />
             </Routes>
           </BrowserRouter>
         );
